6e60d943876d3652920e912602548220e8395b9e
root <root@control.lab.bitbull.ch>
2020-12-07 09:08:12 +0100

    bugfixed and tested with v20

--- tests/nc_benchmark.sh
+++ tests/nc_benchmark.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #########################################################################################
-# DESC: list all nextcloud shares
+# DESC: nextcloud webdav benchmark testet with v20
 #########################################################################################
 # Copyright (c) Chris Ruettimann <chris@bitbull.ch>
 
@@ -20,104 +20,97 @@
 #         USR
 #   4) /usr/local/sbin/nc_benchmark.sh
 #   5) optionally create cronjob to update results on a sheduled base
+#   6) optionally create config file t source the settings
 
 # USEAGE:
 #         nc_benchmark.sh
 
 
 # custom vars
-CLOUD=cloud.template.com
-PW="template-password"
-USR="template-user"
+CLOUD=cloud.domain.ch
+USR="bench-user"
+PW="bench-pass"
 TEST_BLOCK_SIZE_MB=150
-TEST_FILES_COUNT=250
+TEST_FILES_COUNT=100
 BDIR="bench"
 
-cd
-cd bin
-cd(dirname0)
+cd $(dirname $0)
 
-if [ -r "$( basename0).conf" ]
+if [ -r "$(basename $0).conf" ]
 then
-   echo "INFO: reading external config file:( basename0).conf"
-   source "$( basename0).conf"
+   echo "INFO: reading external config file: $(basename $0).conf"
+   source "$(basename $0).conf"
 fi
 
 # static vars
 BURL="https://$CLOUD"
-WURL="$BURL/remote.php/webdav"
+WURL="$BURL/remote.php/dav/files/$USR/"
 TURL="$BURL/remote.php/dav/trashbin/$USR/trash"
 LDIR="$HOME/.nc/$CLOUD"
-LLOG="$LDIR/$(basename0).txt"
+LLOG="$LDIR/$(basename $0).txt"
 RDIR="$WURL/$BDIR"
-CURL="curl -k -s -uUSR:$PW"
+CURL="curl -k -s -u$USR:$PW"
+
 
 # prepare local benchmark dirs
 test -d "$LDIR" && rm -rf "$LDIR"
 mkdir -p "$LDIR/files"
 dd if=/dev/urandom of="$LDIR/$TEST_BLOCK_SIZE_MB.mb" bs=1M count=$TEST_BLOCK_SIZE_MB >/dev/null 2>&1
-for i in(seq 1TEST_FILES_COUNT)
+for i in $(seq 1 $TEST_FILES_COUNT)
 do
-   date >LDIR/files/$i.txt
+   date > $LDIR/files/$i.txt
 done
 
 # prepare remote benchmark dirs
-$CURLRDIR/$(basenameLLOG) -o "$LLOG" 2>/dev/null
-cat "$LLOG" 2>/dev/null | grep -q DATE || echo '#DATE;URL;USER;<UPLOAD|DOWNLOAD>;TEST;ERRORS;RESULTS' > LLOG
-$CURL "$RDIR/files/0.txt"  >/dev/null 2>&1 &&CURLRDIR/CURL -X DELETE "$RDIR/files/" >/dev/null 2>&1
+$CURL $RDIR/$(basename $LLOG) -o "$LLOG" 2>/dev/null 
+cat "$LLOG" 2>/dev/null | grep -q DATE || echo '#DATE;URL;USER;<UPLOAD|DOWNLOAD>;TEST;ERRORS;RESULTS' >  $LLOG
+$CURL "$RDIR/files/0.txt"  >/dev/null 2>&1 && $CURL -X DELETE "$RDIR/files/" >/dev/null 2>&1
 $CURL -X MKCOL "$RDIR" >/dev/null 2>&1
 $CURL -X MKCOL "$RDIR/files" >/dev/null 2>&1
 $CURL -X DELETE "$RDIR/$TEST_BLOCK_SIZE_MB.mb" >/dev/null 2>&1
 
 # run block upload test
-echo uploadTEST_BLOCK_SIZE_MB MB
+echo upload $TEST_BLOCK_SIZE_MB MB
 UL_BLOCK_SPEED=$($CURL -w '%{speed_upload}' -T "$LDIR/$TEST_BLOCK_SIZE_MB.mb" "$RDIR/" | cut -d. -f1)
-UL_BLOCK_SPEED=$((UL_BLOCK_SPEED / 1024 )) # kbyte per sec
+UL_BLOCK_SPEED=$(( $UL_BLOCK_SPEED / 1024 )) # kbyte per sec
 rm -f "$LDIR/$TEST_BLOCK_SIZE_MB.mb"
 # run block download test
-echo downloadTEST_BLOCK_SIZE_MB MB
+echo download $TEST_BLOCK_SIZE_MB MB
 DL_BLOCK_SPEED=$($CURL -w '%{speed_download}' "$RDIR/$TEST_BLOCK_SIZE_MB.mb" -o "$LDIR/$TEST_BLOCK_SIZE_MB.mb" | cut -d. -f1)
-DL_BLOCK_SPEED=$((DL_BLOCK_SPEED / 1024 )) # kbyte per sec
+DL_BLOCK_SPEED=$(( $DL_BLOCK_SPEED / 1024 )) # kbyte per sec
 rm -f "$LDIR/$TEST_BLOCK_SIZE_MB.mb"
 
 # run small file upload test
 UL_ERROR_CNT=0
 TIME_BEFORE=$(date '+%s')
-for i in(seq 1TEST_FILES_COUNT)
+for i in $(seq 1 $TEST_FILES_COUNT)
 do
-   echo upload filei.txt | egrep '[0-9]0.txt'
-  CURL  -T "$LDIR/files/$i.txt" "$RDIR/files/"
-   if [? -ne 0 ] ; then
-      echo "error: could not uploadi.txt"
+   echo upload file $i.txt | egrep '[0-9]0.txt'
+   $CURL  -T "$LDIR/files/$i.txt" "$RDIR/files/" 
+   if [ $? -ne 0 ] ; then
+      echo "error: could not upload $i.txt"
       UL_ERROR_CNT=$(($UL_ERROR_CNT+1))
    fi
 done
-UL_FILES_TIME=$(((date '+%s') -TIME_BEFORE))
+UL_FILES_TIME=$(( $(date '+%s') - $TIME_BEFORE))
 
 # run small file download test
 rm -fr "$LDIR/files"
 mkdir -p "$LDIR/files"
 DL_ERROR_CNT=0
 TIME_BEFORE=$(date '+%s')
-for i in(seq 1TEST_FILES_COUNT)
+for i in $(seq 1 $TEST_FILES_COUNT)
 do
-   echo download filei.txt | egrep '[0-9]0.txt'
-  CURL  -o "$LDIR/files/$i.txt" "$RDIR/files/$i.txt"
-   if [? -ne 0 ] ; then
-      echo "error: could not downloadi.txt"
+   echo download file $i.txt | egrep '[0-9]0.txt'
+   $CURL  -o "$LDIR/files/$i.txt" "$RDIR/files/$i.txt" 
+   if [ $? -ne 0 ] ; then
+      echo "error: could not download $i.txt"
       DL_ERROR_CNT=$(($DL_ERROR_CNT+1))
    fi
 done
-DL_FILES_TIME=$(((date '+%s') -TIME_BEFORE))
+DL_FILES_TIME=$(( $(date '+%s') - $TIME_BEFORE))
 
-# empty trash bin
-$CURL -X PROPFIND "$TURL" | sed -r 's@</?d:href>@\n@g' | sed '/d:prop/d' | grep "/$USR/" | grep "/$BDIR\." | while read trashf
-do
-   echo "INFO: DELETE TRASH FOLDER:trashf for user:USR"
-  CURL -X DELETE "$BURL/$trashf"
-done
-
-echo WURL=$WURL
+echo URL=$URL
 echo TEST_BLOCK_SIZE_MB=$TEST_BLOCK_SIZE_MB
 echo UL_BLOCK_SPEED=$UL_BLOCK_SPEED KByte/s
 echo DL_BLOCK_SPEED=$DL_BLOCK_SPEED KByte/s
@@ -128,15 +121,12 @@ echo UL_FILES_TIME=$UL_FILES_TIME sec
 echo DL_FILES_TIME=$DL_FILES_TIME sec
 
 D="$(date '+%Y.%m.%d %H:%M:%S')"
-echo "$D;$WURL;$USR;UPLOAD;BlockTEST_BLOCK_SIZE_MB MB;;$UL_BLOCK_SPEED KByte/s" >> LLOG
-echo "$D;$WURL;$USR;DOWNLOAD;BlockTEST_BLOCK_SIZE_MB MB;;$DL_BLOCK_SPEED KByte/s" >> LLOG
-echo "$D;$WURL;$USR;UPLOAD;$TEST_FILES_COUNT small Files;$UL_ERROR_CNT;$UL_FILES_TIME sec" >> LLOG
-echo "$D;$WURL;$USR;DOWNLOAD;$TEST_FILES_COUNT small Files;$DL_ERROR_CNT;$DL_FILES_TIME sec" >> LLOG
-echo uploading results:LLOG to webdav
+echo "$D;$URL;$USR;UPLOAD;Block $TEST_BLOCK_SIZE_MB MB;;$UL_BLOCK_SPEED KByte/s" >>  $LLOG
+echo "$D;$URL;$USR;DOWNLOAD;Block $TEST_BLOCK_SIZE_MB MB;;$DL_BLOCK_SPEED KByte/s" >>  $LLOG
+echo "$D;$URL;$USR;UPLOAD;$TEST_FILES_COUNT small Files;$UL_ERROR_CNT;$UL_FILES_TIME sec" >>  $LLOG
+echo "$D;$URL;$USR;DOWNLOAD;$TEST_FILES_COUNT small Files;$DL_ERROR_CNT;$DL_FILES_TIME sec" >>  $LLOG
+echo uploading results: $LLOG to webdav
 $CURL  -T "$LLOG" "$RDIR/"
 echo done
-
-
-
 
 
